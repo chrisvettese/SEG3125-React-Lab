029952063763fb7539859fde96b8e3f437b938aa
Chris Vettese <cvett018@uottawa.ca>
2020-07-11 04:17:06 -0400

    Optimized recipe code

--- src/Recipes.js
+++ src/Recipes.js
@@ -38,60 +38,56 @@ function Divide() {
 }
 
 function Recipes() {
+    const path = window.location.pathname.substring(9)
+    const rIndex = recipeData.recipePaths.indexOf(path);
+
+    let ratingAvg = 0;
+    recipeReviews.ratings[rIndex].forEach(rating => {
+        ratingAvg += rating;
+    });
+    ratingAvg /= recipeReviews.ratings[rIndex].length;
+    ratingAvg = (Math.round(ratingAvg * 10) / 10).toFixed(1)
+    const ratingAvgNum = parseFloat(ratingAvg);
+    const reviewWord = recipeReviews.ratings[rIndex].length === 1 ? " review" : " reviews";
+
     return (
-        recipeData.recipePaths.map(rPath => {
-            const path = window.location.pathname;
-            const rIndex = recipeData.recipePaths.indexOf(path.substring(9));
-
-            let ratingAvg = 0;
-            recipeReviews.ratings[rIndex].forEach(rating => {
-                ratingAvg += rating;
-            });
-            ratingAvg /= recipeReviews.ratings[rIndex].length;
-            ratingAvg = (Math.round(ratingAvg * 10) / 10).toFixed(1)
-            const ratingAvgNum = parseFloat(ratingAvg);
-            const reviewWord = recipeReviews.ratings[rIndex].length === 1 ? " review" : " reviews";
-
-            return (
-                <Route path={"/recipes/" + rPath} key={rPath}>
-                    <NavBar/>
-                    <Typography align="center" variant="h3">{recipeData.recipeNames[rIndex] + "\u00a0"}</Typography>
-                    <Grid container justify="center">
-                        <Rating value={ratingAvgNum} precision={0.1} readOnly/>
-                        <Divider orientation="vertical" flexItem/>
-                        <Typography>{"\u00a0"}</Typography>
-                        <HashLink to={window.location.pathname + "#reviews"}>
-                            <Typography>{recipeReviews.ratings[rIndex].length + reviewWord}</Typography>
-                        </HashLink>
-                    </Grid>
-                    <br/>
-                    <Grid container justify="center">
-                        <img src={recipeData.recipeImages[rIndex]} alt={recipeData.recipeNames[rIndex]}
-                             style={styles.recipeImage}/>
-                    </Grid>
-                    <br/>
-                    <Typography style={styles.recipeParagraph}>{recipeData.recipeParagraphs[rIndex]}</Typography>
-                    <Divide/>
-                    <Typography style={styles.recipeStandard} variant="h4">Ingredients</Typography>
-                    <Typography style={styles.recipeParagraph}>{recipeData.recipeIngredients[rIndex]}</Typography>
-                    <Divide/>
-                    <Typography style={styles.recipeStandard} variant="h4">Instructions</Typography>
-                    <Typography style={styles.recipeParagraph}>{recipeData.recipeInstructions[rIndex]}</Typography>
-                    <Divide/>
-                    <Typography style={styles.recipeStandard} variant="h4" id="reviews">Reviews</Typography>
-                    <Grid container style={styles.recipeStandard}>
-                        <Typography>{ratingAvg + "\u00a0"}</Typography>
-                        <Divider orientation="vertical" flexItem/>
-                        <Typography>{"\u00a0"}</Typography>
-                        <Rating value={ratingAvgNum} precision={0.1} readOnly/>
-                        <Typography>{"\u00a0"}</Typography>
-                        <Divider orientation="vertical" flexItem/>
-                        <Typography>{"\u00a0" + recipeReviews.ratings[rIndex].length + reviewWord}</Typography>
-                    </Grid>
-                </Route>
-            )
-        })
-    );
+        <Route path={"/recipes/" + path} key={path}>
+            <NavBar/>
+            <Typography align="center" variant="h3">{recipeData.recipeNames[rIndex] + "\u00a0"}</Typography>
+            <Grid container justify="center">
+                <Rating value={ratingAvgNum} precision={0.1} readOnly/>
+                <Divider orientation="vertical" flexItem/>
+                <Typography>{"\u00a0"}</Typography>
+                <HashLink to={window.location.pathname + "#reviews"}>
+                    <Typography>{recipeReviews.ratings[rIndex].length + reviewWord}</Typography>
+                </HashLink>
+            </Grid>
+            <br/>
+            <Grid container justify="center">
+                <img src={recipeData.recipeImages[rIndex]} alt={recipeData.recipeNames[rIndex]}
+                     style={styles.recipeImage}/>
+            </Grid>
+            <br/>
+            <Typography style={styles.recipeParagraph}>{recipeData.recipeParagraphs[rIndex]}</Typography>
+            <Divide/>
+            <Typography style={styles.recipeStandard} variant="h4">Ingredients</Typography>
+            <Typography style={styles.recipeParagraph}>{recipeData.recipeIngredients[rIndex]}</Typography>
+            <Divide/>
+            <Typography style={styles.recipeStandard} variant="h4">Instructions</Typography>
+            <Typography style={styles.recipeParagraph}>{recipeData.recipeInstructions[rIndex]}</Typography>
+            <Divide/>
+            <Typography style={styles.recipeStandard} variant="h4" id="reviews">Reviews</Typography>
+            <Grid container style={styles.recipeStandard}>
+                <Typography>{ratingAvg + "\u00a0"}</Typography>
+                <Divider orientation="vertical" flexItem/>
+                <Typography>{"\u00a0"}</Typography>
+                <Rating value={ratingAvgNum} precision={0.1} readOnly/>
+                <Typography>{"\u00a0"}</Typography>
+                <Divider orientation="vertical" flexItem/>
+                <Typography>{"\u00a0" + recipeReviews.ratings[rIndex].length + reviewWord}</Typography>
+            </Grid>
+        </Route>
+    )
 }
 
 export default Recipes;
