bea5b05b29ad60405577d65ead347b1b531caec3
Chris Vettese <cvett018@uottawa.ca>
2020-07-16 21:39:28 -0400

    Updates

---
+++ precache-manifest.ede178dc6c8558f2797a6f28e84e1fdf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba0070f8cbd40eb64a6c48163831e64",
+    "url": "/seg3125-react-lab/index.html"
+  },
+  {
+    "revision": "b618e2125c824f4ab524",
+    "url": "/seg3125-react-lab/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "7aecb983c48b2d62612c",
+    "url": "/seg3125-react-lab/static/js/2.30531f5c.chunk.js"
+  },
+  {
+    "revision": "6a2bc4dfc852217704ff5945e37d112d",
+    "url": "/seg3125-react-lab/static/js/2.30531f5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b618e2125c824f4ab524",
+    "url": "/seg3125-react-lab/static/js/main.744dd5c1.chunk.js"
+  },
+  {
+    "revision": "275ba226622359e4527a",
+    "url": "/seg3125-react-lab/static/js/runtime-main.eb27dfb5.js"
+  },
+  {
+    "revision": "2b615e27bc9f878e36f4be9f3ca47cc0",
+    "url": "/seg3125-react-lab/static/media/chocolateChip.2b615e27.jpg"
+  },
+  {
+    "revision": "1fef628825ec68158256e6a201b1a4a6",
+    "url": "/seg3125-react-lab/static/media/cinnamonRaisin.1fef6288.jpg"
+  },
+  {
+    "revision": "ea5b4e59901e14036643314b4057bacd",
+    "url": "/seg3125-react-lab/static/media/cinnamonRolls.ea5b4e59.jpg"
+  },
+  {
+    "revision": "5ff1f8209d1213dad0050530f7214fe7",
+    "url": "/seg3125-react-lab/static/media/glutenFree.5ff1f820.png"
+  },
+  {
+    "revision": "182afd5c67c87ba2ba97a203f5d8c059",
+    "url": "/seg3125-react-lab/static/media/logo.182afd5c.png"
+  },
+  {
+    "revision": "c05089c09c727bd56c74c42b9040ad0e",
+    "url": "/seg3125-react-lab/static/media/molassesCookies.c05089c0.jpg"
+  },
+  {
+    "revision": "2c563aa3954be9b38d43dee0c353ecdd",
+    "url": "/seg3125-react-lab/static/media/sugarCookies.2c563aa3.jpg"
+  },
+  {
+    "revision": "3d4ff5b362e4725216a9a96aeb2099a7",
+    "url": "/seg3125-react-lab/static/media/vegan.3d4ff5b3.png"
+  },
+  {
+    "revision": "1afee3ff2ef7740573bec8afdf0723cc",
+    "url": "/seg3125-react-lab/static/media/whiteBread.1afee3ff.jpg"
+  }
+]);
